1c44258b8c1c2acb7057623dff268523e41a5560
Jalfaro1010 <joshua.alfaro@gmail.com>
2023-11-09 13:32:48 -0500

    Finishedfizzbuzz challenge

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
 # add your code here
-
+for num in range (100):
+    if num % 3 == 0 and num % 5 == 0:
+        print ("fizzbuzz")
+    elif num % 3 == 0:
+        print ("fizz")
+    elif num % 5 == 0:
+        print ("buzz")
+    else: 
+        print (num)
